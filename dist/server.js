644af1c82bb1978e9376e3d5cdf483027d16ec6d
SerjeiMikailov <felipemaders8@gmail.com>
2023-02-04 12:10:02 -0300

    gg

--- dist/server.js
+++ dist/server.js
@@ -7,6 +7,10 @@ const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./routes/index"));
 const panel_1 = __importDefault(require("./routes/panel"));
 const server = (0, express_1.default)();
-server.listen(1919);
+server.use(express_1.default.static('public'));
 server.use(index_1.default);
 server.use('/panel', (panel_1.default));
+server.use((req, res) => {
+    res.status(404).send('Error 404 - Page not found');
+});
+server.listen(1919);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express, {Request, Response} from 'express';
+import path from 'path';
 import mainroutes from './routes/index'
 import panelroutes from './routes/panel'
 
 const server = express()
-server.listen(1919)
+
+server.use(express.static('public'))
 
 server.use(mainroutes)
+
 server.use('/panel', (panelroutes))
+
+server.use((req: Request, res: Response)=> {
+    res.status(404).send('Error 404 - Page not found')
+})
+
+server.listen(1919)
